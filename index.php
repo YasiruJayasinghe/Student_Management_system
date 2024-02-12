2ba74af5bbb51f73c1bee0f59811533d93ceea08
YasiruJayasinghe <jayasinghejmys.20@uom.lk>
2024-02-12 22:42:06 +0530

    Your commit message here

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Student Management System</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+<body>
+   <nav>
+    <label class="logo">Wishwa Education Academy</label>
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Contact</a></li>
+        <li><a href="">Admission</a></li>
+        <li><a href="" class="btn btn-success">Login</a></li>
+    </ul>
+   </nav>
+
+   <div class="section1">
+   	<label class="img_text">We Teach Students With Care</label>
+   	<img src= "school_management.jpg" class="main_img">
+   </div>
+
+   <div class="container">
+   	<div class="row">
+   		<div class="col-md-4">
+   			<img src="school2.jpg" class="welcome_img">
+   		</div>
+   		<div class="col-md-8">
+   			<h1>Welcome to Wishwa Education Academy </h1>
+   			<p>MEMS has been committed to global learning long before it became an indispensable feature of contemporary education. Established in 1997, we proudly stand as the 1st English medium school in Sri Lanka to adopt both Pearson Edexcel and Cambridge curriculum (in O and A levels), drawing together students in a vibrant, academically challenging, and encouraging environment where manifold viewpoints are prized and celebrated.MEMS has been committed to global learning long before it became an indispensable feature of contemporary education. Established in 1997, we proudly stand as the 1st English medium school in Sri Lanka to adopt both Pearson Edexcel and Cambridge curriculum (in O and A levels), drawing together students in a vibrant, academically challenging, and encouraging environment where manifold viewpoints are prized and celebrated.</p>
+   		</div> 		
+   	</div>  	
+   </div>
+
+   <center>
+   	<h1>Our Teachers</h1>
+   </center>
+
+   <div class="container">
+   	<div class="row">
+   		<div class="col-md-4">
+   			<img class="teacher" src="teacher1.jpg">
+   			<p>in a vibrant, academically challenging, and encouraging environment where manifold viewpoints are prized and celebrated.</p>
+   		</div>
+   		<div class="col-md-4">
+   			<img class="teacher" src="teacher2.jpg">
+   			<p>in a vibrant, academically challenging, and encouraging environment where manifold viewpoints are prized and celebrated.</p>
+   		</div>
+   		<div class="col-md-4">
+   			<img class="teacher" src="teacher3.jpg">
+   			<p>in a vibrant, academically challenging, and encouraging environment where manifold viewpoints are prized and celebrated.</p>
+   		</div>
+   	</div>
+   </div>
+
+
+      <center>
+   	<h1>Our Courses</h1>
+   </center>
+
+   <div class="container">
+   	<div class="row">
+   		<div class="col-md-4">
+   			<img class="teacher" src="web.jpg">
+   			<h3>Web Development</h3>
+   		</div>
+   		<div class="col-md-4">
+   			<img class="teacher" src="graphic.jpg">
+   			<h3>Graphics Design</h3>
+   		</div>
+   		<div class="col-md-4">
+   			<img class="teacher" src="marketing.png">
+   			<h3>Marketing</h3>
+   		</div>
+   	</div>
+   </div>
+
+   <div class="adm">
+   	<center><h1>Admission Form</h1></center>
+   	
+   	   <div class="admission_form" align="center">
+   	<form>
+   		<div class="adm_int">
+   			<label class="label_text">Name</label>
+   			<input class="input_deg" type="text" name="name">
+   		</div>
+   		<div class="adm_int">
+   			<label class="label_text">Email</label>
+   			<input class="input_deg" type="Email" name="email">
+   		</div>
+   		<div class="adm_int">
+   			<label class="label_text">Phone</label>
+   			<input class="input_deg" type="text" name="phone">
+   		</div>
+   		<div class="adm_int">
+   			<label class="label_text">Message</label>
+   			<textarea class="input_txt"></textarea>
+   		</div>
+   		<div class="adm_int">
+   			<input class="btn btn-primary" type="submit" id="submit" value="apply">
+   		</div>
+   	</form>
+   </div>
+   </div>
+
+
+   <footer>
+   	<h3 class="footer_txt">All @copyright reserved by web tech knowledge</h3>
+   </footer>
+
+   <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
